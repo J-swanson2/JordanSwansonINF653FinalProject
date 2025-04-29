cc07e0abe8893ec53705c877396bc653d99fd5e2
Jordan Swanson <j_swanson2@mail.fhsu.edu>
2025-04-29 08:46:01 -0500

    added states schema

---
+++ model/States.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const stateSchema = new Schema({
+	stateCode: {
+		type: String,
+		require: true
+	},
+	funfacts: [String]
+})
