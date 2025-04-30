2ad8d321f87dbc964a658c168bb228eaa78a16b8
Jordan Swanson <j_swanson2@mail.fhsu.edu>
2025-04-30 09:26:57 -0500

    Added CORS and a whitelist

--- server.js
+++ server.js
@@ -2,11 +2,25 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const logEvents = require('./middleware/logEvents');
 const PORT = 3500;
 
 //custom middleware logger
 app.use(logEvents);
+
+const whitelist = ['localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 //middleware for handling getting form data
 app.use(express.urlencoded({ extended: false }));
